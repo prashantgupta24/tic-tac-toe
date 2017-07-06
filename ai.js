c8c97fcae68c600707987ad12a71128d231571f4
Prashant Gupta <prashantgupta24@gmail.com>
2017-07-05 22:57:13 -0700

    integrated Ai (#1)
    integrated AI

---
+++ ai.js
@@ -0,0 +1,145 @@
+function createTicTacToeAI(ticTacToeGame) {
+
+  let arrayElem = ticTacToeGame.getArrayElem();
+  let size = ticTacToeGame.getSize();
+
+  return {
+
+    findBestMove: function() {
+      let bestMove = 0;
+      let bestMoveValMax = -1000;
+      let bestMoveValMin = 1000;
+
+      let isMaxMove = ticTacToeGame.getTurn() === ticTacToeGame.turnEnum.CROSS ? true : false;
+      for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+          if (arrayElem[i][j] === 0) {
+            arrayElem[i][j] = isMaxMove ? 1 : -1;
+            let val = this.minimax(!isMaxMove, 0);
+            if (isMaxMove) {
+              if (val >= bestMoveValMax) {
+                bestMoveValMax = val;
+                bestMove = (3 * i) + (j + 1);
+              }
+            } else {
+              if (val <= bestMoveValMin) {
+                bestMoveValMin = val;
+                bestMove = (3 * i) + (j + 1);
+              }
+            }
+            arrayElem[i][j] = 0;
+          }
+        }
+      }
+      return bestMove;
+    },
+
+    minimax: function(isMaxMove, depth) {
+
+      //if game finished
+      let val = checkGame();
+      if (Math.abs(val) === 3) {
+        if (val === 3) {
+          return 10 - depth;
+        } else {
+          return depth - 10;
+        }
+      }
+
+      if (!movesLeft()) {
+        return 0;
+      }
+
+      if (isMaxMove) {
+        let bestVal = -1000;
+        for (let i = 0; i < size; i++) {
+          for (let j = 0; j < size; j++) {
+            if (arrayElem[i][j] === 0) {
+              arrayElem[i][j] = 1;
+              //ticTacToeGame.turn = ticTacToeGame.turnEnum.CIRCLE;
+              bestVal = Math.max(this.minimax(!isMaxMove, depth+1), bestVal);
+              arrayElem[i][j] = 0;
+            }
+          }
+        }
+        return bestVal;
+      } else {
+        let bestVal = 1000;
+        for (let i = 0; i < size; i++) {
+          for (let j = 0; j < size; j++) {
+            if (arrayElem[i][j] === 0) {
+              arrayElem[i][j] = -1;
+              //ticTacToeGame.turn = ticTacToeGame.turnEnum.CROSS;
+              bestVal = Math.min(this.minimax(!isMaxMove, depth+1), bestVal);
+              arrayElem[i][j] = 0;
+            }
+          }
+        }
+        return bestVal;
+      }
+    }
+  };
+
+
+  function checkGame() {
+
+    //rows
+    for (let i = 0; i < size; i++) {
+      let val = 0;
+      for (let j = 0; j < size; j++) {
+        val += arrayElem[i][j];
+        if (Math.abs(val) === 3) {
+          return val;
+        }
+      }
+    }
+
+    //columns
+    for (let i = 0; i < size; i++) {
+      let val = 0;
+      for (let j = 0; j < size; j++) {
+        val += arrayElem[j][i];
+        if (Math.abs(val) === 3) {
+          return val;
+        }
+      }
+    }
+
+    //d1
+    let val = 0;
+    for (let i = 0; i < size; i++) {
+      for (let j = 0; j < size; j++) {
+        if (i === j) {
+          val += arrayElem[i][j];
+        }
+        if (Math.abs(val) === 3) {
+          return val;
+        }
+      }
+    }
+
+    //d2
+    val = 0;
+    for (let i = 0; i < size; i++) {
+      for (let j = 0; j < size; j++) {
+        if (i + j === 2) {
+          val += arrayElem[i][j];
+        }
+        if (Math.abs(val) === 3) {
+          return val;
+        }
+      }
+    }
+
+  }
+
+  function movesLeft() {
+    for (let i = 0; i < size; i++) {
+      for (let j = 0; j < size; j++) {
+        if (arrayElem[i][j] === 0)
+          return true;
+      }
+    }
+    return false;
+  }
+}
--- main.js
+++ main.js
@@ -1,196 +1,283 @@
-$(function () {
+$(function() {
   //console.log('ready');
-  let ticTacToeGame = ticTacToe;
-
-  ticTacToeGame.canvas = {
-    canvasElement: document.getElementById('wordCanvas'),
-    ctx: document.getElementById('wordCanvas').getContext('2d'),
-    initializeCanvas: function(numRows) {
-      let numRowsForGrid = numRows + 2;
-      let widthPerColumn = Math.floor(this.canvasElement.width/numRowsForGrid);
-      let heightPerRow = Math.floor(this.canvasElement.height/numRowsForGrid);
-      //console.log(heightPerRow);
-
-      for(let i=1;i<numRowsForGrid;i++) {
-        //console.log(i*widthPerColumn);
-        ticTacToeGame.grid['x'+(i)] = i*widthPerColumn;
-      }
-
-      for(let i=1;i<numRowsForGrid;i++) {
-        //console.log(i*heightPerRow);
-        ticTacToeGame.grid['y'+(i)] = i*heightPerRow;
-      }
-
-      //horizontal lines
-      for(let i=2;i<=numRows;i++) {
-        let p1 = new Point(ticTacToeGame.grid.x1, ticTacToeGame.grid['y'+(i)]);
-        let p2 = new Point(ticTacToeGame.grid['x'+(numRows+1)], ticTacToeGame.grid['y'+(i)]);
-        this.drawPoints(p1, p2);
-      }
-
-      //vertical lines
-      for(let i=2;i<=numRows;i++) {
-        let p1 = new Point(ticTacToeGame.grid['x'+(i)], ticTacToeGame.grid.y1);
-        let p2 = new Point(ticTacToeGame.grid['x'+(i)], ticTacToeGame.grid['y'+(numRows+1)]);
-        this.drawPoints(p1, p2);
-      }
-    },
-    drawPoints: function(p1, p2) {
-      this.ctx.moveTo(p1.x,p1.y);
-      this.ctx.lineTo(p2.x,p2.y);
-      this.ctx.stroke();
-    },
-    drawCross: function(p1, p2, p3, p4) {
-      this.drawPoints(p1, p4);
-      this.drawPoints(p2, p3);
-    },
-    drawCircle: function(p1, p2, p3, p4) {
-      let centreX = (p1.x + p4.x)/2;
-      let centreY = (p1.y + p4.y)/2;
-      let radius = (p4.x - p1.x) < (p4.y - p1.y)? (p4.x - p1.x)/2 : (p4.y - p1.y)/2;
-
-      this.ctx.beginPath();
-      this.ctx.arc(centreX,centreY,radius,0,2*Math.PI);
-      this.ctx.stroke();
-    },
-    clearCanvas: function () {
-      this.ctx.clearRect(0, 0, this.canvasElement.width, this.canvasElement.height);
-      //console.log('canvas cleared');
-    }
-  };
-
-  ticTacToeGame.chancesElem = $('#chances');
+  let ticTacToeGame = ticTacToe(3); //main value, sets size of the game board (size * size)
   ticTacToeGame.initialize();
 
-  $(document).keypress(function (event) {
-    if(ticTacToeGame.playing) {
+  let ticTacToeAI = createTicTacToeAI(ticTacToeGame);
+
+  $(document).keypress(function(event) {
+    if (ticTacToeGame.isPlaying()) {
       let keyCode = event.which || event.keyCode;
       //console.log(keyCode);
-      if(keyCode >= 49 && keyCode <= 57) {
-        ticTacToeGame.playMove(String.fromCharCode(keyCode));
+      let validMove = false;
+      if (keyCode >= 49 && keyCode <= 57) {
+        validMove = ticTacToeGame.playMove(String.fromCharCode(keyCode));
+      }
+      if (ticTacToeGame.isPlaying() && validMove) {
+        let moveByAI = ticTacToeAI.findBestMove();
+        ticTacToeGame.playMove(moveByAI);
       }
     }
   });
+
+  // let arrayElem = ticTacToeGame.getArrayElem();
+  // //arrayElem[0][0]=1;
+  // arrayElem[0][1]=1;
+  // //arrayElem[0][2]=1;
+  //
+  // //arrayElem[1][0]=-1;
+  // //arrayElem[1][1]=-1;
+  // arrayElem[1][2]=1;
+  //
+  // arrayElem[2][0]=-1;
+  // arrayElem[2][1]=-1;
+  // arrayElem[2][2]=1;
+  //
+  // let ticTacToeAI = createTicTacToeAI(ticTacToeGame);
+  // console.log(ticTacToeAI.findBestMove());
+
+  function ticTacToe(size) {
+
+    let turn = '';
+    let grid = {};
+    let canvas = {};
+    let arrayElem = [];
+    let chancesElem = '';
+    let playing = false;
+    let rows = [];
+    let columns = [];
+    let d1 = 0;
+    let d2 = 0;
+    let numMoves = 0;
+
+    function getSize() {
+      return size;
+    }
+
+    function isPlaying() {
+      return playing;
+    }
+
+    function setPlaying(isPlaying) {
+      playing = isPlaying;
+    }
+
+    function getTurn() {
+      return turn;
+    }
+
+    function setTurn(setTurn) {
+      turn = setTurn;
+    }
+
+    function getArrayElem() {
+      return arrayElem;
+    }
+
+    let turnEnum = {
+      CROSS: 'CROSS',
+      CIRCLE: 'CIRCLE',
+      getValue: function(turn) {
+        if (turn === this.CROSS) {
+          return 1;
+        } else {
+          return -1;
+        }
+      }
+    };
+
+    function initialize() {
+      canvas = createCanvas(grid);
+      canvas.initializeCanvas(size);
+      arrayElem = arrayMatrix(size, size, 0),
+        chancesElem = $('#chances');
+      setPlaying(true);
+      setTurn(turnEnum.CIRCLE);
+      numMoves = 0;
+      chancesElem.html(getTurn());
+      initializeArrays();
+    }
+
+    function initializeArrays() {
+      for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+          arrayElem[i][j] = 0;
+          rows[j] = 0;
+          columns[j] = 0;
+        }
+      }
+    }
+
+    function playMove(quad) {
+
+      let xVal = Math.floor((quad - 1) / size);
+      let yVal = Math.floor((quad - 1)) % size;
+
+      if (arrayElem[xVal][yVal] != 0) {
+        alert('That spot is filled!');
+        return false;
+      } else {
+        numMoves += 1;
+        arrayElem[xVal][yVal] = getTurn() === turnEnum.CROSS ? 1 : -1;
+        drawObject(yVal, xVal);
+        let gameWon = checkGame(xVal, yVal);
+
+        if (gameWon) {
+          setTimeout(function() {
+            alert(getTurn() + ' won!');
+          }, 100);
+          setPlaying(false);
+        } else if (numMoves === size * size) {
+          setTimeout(function() {
+            alert('Game draw!');
+          }, 100);
+          setPlaying(false);
+        } else {
+          setTurn(getTurn() === turnEnum.CIRCLE ? turnEnum.CROSS : turnEnum.CIRCLE);
+          chancesElem.html(getTurn());
+        }
+        return true;
+      }
+    }
+
+    function checkGame(i, j) {
+      let val = turnEnum.getValue(getTurn());
+      rows[i] += val;
+      columns[j] += val;
+      if (i === j) {
+        d1 += val;
+      }
+      if ((i + j) === size - 1) {
+        d2 += val;
+      }
+      // console.log('R : '+rows);
+      // console.log('C : ' + columns);
+      // console.log('d1 : ' + d1);
+      // console.log('d2 : ' + d2);
+
+      if (Math.abs(rows[i]) === size || Math.abs(columns[j]) === size ||
+        Math.abs(d1) === size || Math.abs(d2) === size) {
+        return true;
+      }
+    }
+
+    function drawObject(xVal, yVal) {
+
+      // let xVal = Math.floor((quad-1))%3;
+      // let yVal = Math.floor((quad-1)/3);
+
+      let threshold = 20;
+
+      let p1 = new point(grid['x' + (xVal + 1)] + threshold, grid['y' + (yVal + 1)] + threshold);
+      let p2 = new point(grid['x' + (xVal + 1)] + threshold, grid['y' + (yVal + 2)] - threshold);
+      let p3 = new point(grid['x' + (xVal + 2)] - threshold, grid['y' + (yVal + 1)] + threshold);
+      let p4 = new point(grid['x' + (xVal + 2)] - threshold, grid['y' + (yVal + 2)] - threshold);
+
+      // console.log('x between ' + (xVal+1) + ' and ' + (xVal+2));
+      // console.log('y between ' + (yVal+1) + ' and ' + (yVal+2));
+      if (getTurn() === turnEnum.CIRCLE) {
+        canvas.drawCircle(p1, p2, p3, p4);
+      } else {
+        canvas.drawCross(p1, p2, p3, p4);
+      }
+    }
+
+    return {
+      getArrayElem: getArrayElem,
+      turnEnum: turnEnum,
+      isPlaying: isPlaying,
+      getSize: getSize,
+      getTurn: getTurn,
+      initialize: initialize,
+      playMove: playMove,
+    };
+  }
+
+  function createCanvas(gameGrid) {
+
+    let canvasElement = document.getElementById('wordCanvas');
+    let ctx = document.getElementById('wordCanvas').getContext('2d');
+
+    function initializeCanvas(numRows) {
+      let numRowsForGrid = numRows + 2;
+      let widthPerColumn = Math.floor(canvasElement.width / numRowsForGrid);
+      let heightPerRow = Math.floor(canvasElement.height / numRowsForGrid);
+      //console.log(heightPerRow);
+
+      for (let i = 1; i < numRowsForGrid; i++) {
+        //console.log(i*widthPerColumn);
+        gameGrid['x' + (i)] = i * widthPerColumn;
+      }
+
+      for (let i = 1; i < numRowsForGrid; i++) {
+        //console.log(i*heightPerRow);
+        gameGrid['y' + (i)] = i * heightPerRow;
+      }
+
+      //horizontal lines
+      for (let i = 2; i <= numRows; i++) {
+        let p1 = new point(gameGrid.x1, gameGrid['y' + (i)]);
+        let p2 = new point(gameGrid['x' + (numRows + 1)], gameGrid['y' + (i)]);
+        drawPoints(p1, p2);
+      }
+
+      //vertical lines
+      for (let i = 2; i <= numRows; i++) {
+        let p1 = new point(gameGrid['x' + (i)], gameGrid.y1);
+        let p2 = new point(gameGrid['x' + (i)], gameGrid['y' + (numRows + 1)]);
+        drawPoints(p1, p2);
+      }
+    }
+
+    function drawPoints(p1, p2) {
+      ctx.moveTo(p1.x, p1.y);
+      ctx.lineTo(p2.x, p2.y);
+      ctx.stroke();
+    }
+
+    function drawCross(p1, p2, p3, p4) {
+      drawPoints(p1, p4);
+      drawPoints(p2, p3);
+    }
+
+    function drawCircle(p1, p2, p3, p4) {
+      let centreX = (p1.x + p4.x) / 2;
+      let centreY = (p1.y + p4.y) / 2;
+      let radius = (p4.x - p1.x) < (p4.y - p1.y) ? (p4.x - p1.x) / 2 : (p4.y - p1.y) / 2;
+
+      ctx.beginPath();
+      ctx.arc(centreX, centreY, radius, 0, 2 * Math.PI);
+      ctx.stroke();
+    }
+
+    function clearCanvas() {
+      ctx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+      //console.log('canvas cleared');
+    }
+
+    return {
+      initializeCanvas: initializeCanvas,
+      drawCross: drawCross,
+      drawCircle: drawCircle,
+      clearCanvas: clearCanvas
+    };
+  }
+
+  function arrayMatrix (numRows, numCols, initial) {
+    var arr = [];
+    for (var i = 0; i < numRows; ++i) {
+      var columns = [];
+      for (var j = 0; j < numCols; ++j) {
+        columns[j] = initial;
+      }
+      arr[i] = columns;
+    }
+    return arr;
+  }
+
+  function point(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
 });
-
-Array.matrix = function (numRows, numCols, initial) {
-  var arr = [];
-  for (var i = 0; i < numRows; ++i){
-    var columns = [];
-    for (var j = 0; j < numCols; ++j){
-      columns[j] = initial;
-    }
-    arr[i] = columns;
-  }
-  return arr;
-};
-
-let ticTacToe = {
-  size: 3, //main value, sets size of the game board (size * size)
-  grid: {},
-  playing: false,
-  turnEnum: {
-    CROSS: 'CROSS',
-    CIRCLE: 'CIRCLE',
-    getValue: function (turn) {
-      if(turn === this.CROSS) {
-        return 1;
-      } else {
-        return -1;
-      }
-    }
-  },
-  turn: '',
-  rows: new Array(this.size),
-  columns: new Array(this.size),
-  d1: 0,
-  d2: 0,
-  numMoves: 0,
-  initialize: function () {
-    this.canvas.initializeCanvas(this.size);
-    this.arrayElem = Array.matrix(this.size,this.size,0),
-    this.playing = true;
-    this.turn = this.turnEnum.CROSS;
-    this.numMoves = 0;
-    this.chancesElem.html(this.turn);
-    this.initializeArrays();
-  },
-  initializeArrays: function () {
-    for(let i=0;i<this.size;i++) {
-      for(let j=0;j<this.size;j++){
-        this.arrayElem[i][j]=0;
-        this.rows[j] = 0;
-        this.columns[j] = 0;
-      }
-    }
-  },
-  playMove: function (quad) {
-
-    let xVal = Math.floor((quad-1)/this.size);
-    let yVal = Math.floor((quad-1))%this.size;
-
-    if(this.arrayElem[xVal][yVal] != 0) {
-      alert('That spot is filled!');
-    } else {
-      this.numMoves += 1;
-      this.arrayElem[xVal][yVal] = 1;
-      this.completeMove(yVal, xVal);
-      let gameWon = this.checkGame(xVal, yVal);
-
-      if(gameWon) {
-        setTimeout(function() {alert(ticTacToe.turn + ' won!');}, 100);
-        this.playing = false;
-      } else if(this.numMoves === this.size*this.size){
-        setTimeout(function() {alert('Game draw!');}, 100);
-      } else {
-        this.turn = this.turn === this.turnEnum.CIRCLE? this.turnEnum.CROSS : this.turnEnum.CIRCLE;
-        this.chancesElem.html(this.turn);
-      }
-    }
-  },
-  checkGame: function (i, j) {
-    let val = this.turnEnum.getValue(this.turn);
-    this.rows[i]+=val;
-    this.columns[j]+=val;
-    if(i === j) {
-      this.d1+=val;
-    }
-    if((i+j) === this.size-1) {
-      this.d2+=val;
-    }
-    // console.log('R : '+this.rows);
-    // console.log('C : ' + this.columns);
-    // console.log('d1 : ' + this.d1);
-    // console.log('d2 : ' + this.d2);
-
-    if(Math.abs(this.rows[i]) === this.size || Math.abs(this.columns[j]) === this.size
-  || Math.abs(this.d1) === this.size || Math.abs(this.d2) === this.size) {
-      return true;
-    }
-  },
-  completeMove: function (xVal, yVal) {
-
-    // let xVal = Math.floor((quad-1))%3;
-    // let yVal = Math.floor((quad-1)/3);
-
-    let threshold = 20;
-
-    let p1 = new Point(this.grid['x'+(xVal + 1)]+threshold, this.grid['y'+(yVal+1)]+threshold);
-    let p2 = new Point(this.grid['x'+(xVal + 1)]+threshold, this.grid['y'+(yVal+2)]-threshold);
-    let p3 = new Point(this.grid['x'+(xVal + 2)]-threshold, this.grid['y'+(yVal+1)]+threshold);
-    let p4 = new Point(this.grid['x'+(xVal + 2)]-threshold, this.grid['y'+(yVal+2)]-threshold);
-
-    // console.log('x between ' + (xVal+1) + ' and ' + (xVal+2));
-    // console.log('y between ' + (yVal+1) + ' and ' + (yVal+2));
-    if(this.turn === this.turnEnum.CIRCLE) {
-      this.canvas.drawCircle(p1, p2, p3, p4);
-    } else {
-      this.canvas.drawCross(p1, p2, p3, p4);
-    }
-  }
-};
-
-let Point = function(x, y) {
-  this.x = x;
-  this.y = y;
-};
