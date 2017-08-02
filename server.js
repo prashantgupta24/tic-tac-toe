a5ffe14ea3cf56aa1701f08dc1ef23a27474d0a2
Prashant Gupta <prashantgupta@us.ibm.com>
2017-08-02 16:42:36 -0700

    refactoring

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var app = express();
 var server = http.Server(app);
 var io = socketIO(server);
 
-app.set('port', 5000);
+app.set('port', (process.env.PORT || 5000));
 app.use('/static', express.static(__dirname + '/static'));
 
 // Routing
@@ -17,7 +17,7 @@ app.get('/', function(request, response) {
 });
 
 // Starts the server.
-server.listen(5000, function() {
+server.listen((process.env.PORT || 5000), function() {
   console.log('Starting server on port 5000');
 });
 
